56cf732ef509c4a19540b5f5f3291a22817325de
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2025-10-16 11:53:17 +0200

    fix(crafting): Make minecraft item definitions use material choice

--- src/main/java/dev/thorinwasher/forgery/recipe/CraftingRecipe.java
+++ src/main/java/dev/thorinwasher/forgery/recipe/CraftingRecipe.java
@@ -6,6 +6,8 @@ import dev.thorinwasher.forgery.forging.ItemAdapter;
 import dev.thorinwasher.forgery.inventory.ForgingItem;
 import dev.thorinwasher.forgery.inventory.ForgingMaterial;
 import dev.thorinwasher.forgery.util.ForgeryKey;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
@@ -56,7 +58,10 @@ public record CraftingRecipe(RecipeResult result, Shape shape) {
                     .toArray(String[]::new);
             recipe.shape(lines);
             for (Map.Entry<Character, ForgeryKey> entry : materials.entrySet()) {
-                recipe.setIngredient(entry.getKey(), compileExact(entry.getValue(), itemAdapter));
+                RecipeChoice choice = entry.getValue().namespace().equals(NamespacedKey.MINECRAFT) ?
+                        new RecipeChoice.MaterialChoice(Registry.MATERIAL.get(entry.getValue().toAdventure().get()))
+                        : compileExact(entry.getValue(), itemAdapter);
+                recipe.setIngredient(entry.getKey(), choice);
             }
             return recipe;
         }
